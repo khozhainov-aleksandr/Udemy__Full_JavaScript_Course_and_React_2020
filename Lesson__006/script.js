4cd8b12ebb9cdf3424c4a372ad6d7ecf8af2b64b
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2020-05-16 20:03:24 +0100

    Finish lesson #6

---
+++ Lesson__006/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+// 
+
+const persona = 'Alex';
+
+// 
+
+const bool = false;
+
+// 
+
+let und;
+console.log(und);
+
+// 
+
+//  > Обект
+const obj = {
+	name: 'John',
+	age: 25,
+	isMarried: false
+}; // ! то что в скобках это и есть Обект.
+// ! Может в себе содержать данные которые будут называться (Свойствами обекта) и так же какте то действия (Методы)
+
+console.log(obj.name);
+// console.log(obj["name"]);
+
+// 
+// -          0            1              2       3  4   5   - Нумерация масива, всегда начинаеться с 0
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
+console.log(arr[2]);
