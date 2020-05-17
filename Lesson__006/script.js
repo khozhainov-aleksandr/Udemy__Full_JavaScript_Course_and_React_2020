96c19471265165b48813f977835e1ccca61f9188
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2020-05-17 11:00:45 +0100

    Finish lesson #6 / Finish working width supplementary material

--- Lesson__006/script.js
+++ Lesson__006/script.js
@@ -1,37 +1,98 @@
 "use strict";
 
-let number = 4.6;
+/*
+	let number = 4.6;
 
-console.log(-4/0);
-console.log('string' * 9);
-
-// 
-
-const persona = 'Alex';
+	console.log(-4/0);
+	console.log('string' * 9);
+*/
 
 // 
 
-const bool = false;
+/*
+	const persona = 'Alex';
+*/
 
 // 
 
-let und;
-console.log(und);
+/*
+	const bool = false;
+*/
 
 // 
 
-//  > Обект
-const obj = {
-	name: 'John',
-	age: 25,
-	isMarried: false
-}; // ! то что в скобках это и есть Обект.
-// ! Может в себе содержать данные которые будут называться (Свойствами обекта) и так же какте то действия (Методы)
-
-console.log(obj.name);
-// console.log(obj["name"]);
+/*
+	let und;
+	console.log(und);
+*/
 
 // 
-// -          0            1              2       3  4   5   - Нумерация масива, всегда начинаеться с 0
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
-console.log(arr[2]);
+
+/*
+	//  > Обект
+	const obj = {
+		name: 'John',
+		age: 25,
+		isMarried: false
+	}; // ! то что в скобках это и есть Обект.
+	// ! Может в себе содержать данные которые будут называться (Свойствами обекта) и так же какте то действия (Методы)
+
+	console.log(obj.name);
+	// console.log(obj["name"]);
+*/
+
+// 
+
+/*
+	// -          0            1              2       3  4   5   - Нумерация масива, всегда начинаеться с 0
+	let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, {}, []];
+	console.log(arr[2]);
+*/
+
+// 
+
+/*
+	let id1 = Symbol('id');
+	let id2 = Symbol('id');
+	console.log(id1 == id2);
+*/
+
+// 
+
+/*
+	let id = Symbol("id");
+	// alert(id);
+	// alert(id.toString()); // - выводит Symbol(id)
+	alert(id.description); // - выводит только id
+*/
+
+// 
+
+/*
+	let user = {
+		name: 'Alex'
+	};
+
+	let id = Symbol("id");
+
+	user[id] = 1;
+
+	alert( user[id] );
+*/
+
+// 
+
+/*
+	let id = Symbol.for("id");
+	let idAgain = Symbol.for("id");
+	alert(id === idAgain); // true
+*/
+
+// 
+
+/*
+	const max = Number.MIN_SAFE_INTEGER;
+	console.log(max - 2);
+*/
+
+// 
