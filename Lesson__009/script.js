80dac7a85d040cdbb1eea27b74572d207b6e9a41
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2020-05-17 22:18:23 +0100

    Not finish lesson #9 because video no working, continue after Three or Fore days

---
+++ Lesson__009/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/*
+	let incr = 10,
+	decr = 10;
+
+	incr++; //> Оператор Инкримента, увеличение на единицу.
+	decr--; //> Оператор Декримента, уменьшение на единицу.
+
+	//> incr++;   Поствиксный
+	//> decr--;
+
+	//> ++incr;   Префиксный 
+	//> --decr;
+
+	console.log(incr);
+	console.log(decr);
+*/
+
+// 
+
+/*
+	console.log(4*2 == '8'); // true
+	console.log(4*2 === '8'); // false
+*/
+
+// 
+
+//> &&   И
+//> ||   ИЛИ
+
+const isChecked = true,
+		isClose = true;
+
+console.log(isChecked && isClose);
