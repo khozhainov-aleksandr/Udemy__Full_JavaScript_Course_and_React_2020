8f5eb679453b1d463fa1fa483dd5a3002b61029c
Aleksandr <sea.man.aleksandr@gmail.com>
2020-06-14 02:22:50 +0300

    small update

--- Lesson__015/script.js
+++ Lesson__015/script.js
@@ -1,37 +1,37 @@
 "use strict";
-
-let number = 20;
-
-function showFirstMessage(text) {
-    console.log(text);
-    let number = 10;
-}
-
-showFirstMessage('Hello world!');
-console.log(number);
-
-
-function calc(a, b) {
-    return (a + b);
-}
-
-console.log(calc(5,3));
-
-
-function ret() {
-    let number = 50;
-    return number;
-}
-console.log(ret());
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-
-const logan = function() {
-    console.log('Hello');
-};
-logan();
-
-const calcSum = (a, b) => a + b;
-console.log(calcSum(8, 8));
+//
+// let number = 20;
+//
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let number = 10;
+// }
+//
+// showFirstMessage('Hello world!');
+// console.log(number);
+//
+//
+// function calc(a, b) {
+//     return (a + b);
+// }
+//
+// console.log(calc(5,3));
+//
+//
+// function ret() {
+//     let number = 50;
+//     return number;
+// }
+// console.log(ret());
+//
+// const anotherNum = ret();
+// console.log(anotherNum);
+//
+//
+// const logan = function() {
+//     console.log('Hello');
+// };
+// logan();
+//
+// const calcSum = (a, b) => a + b;
+// console.log(calcSum(8, 8));
---
+++ Lesson__017/script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+// start();
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Один из просмотренных фильмов ?', '');
+        let b = prompt('На сколько оцените его ?', '');
+
+        if (a !== null && b !== null //x != обозначает что не равно // null возвращает  нам при нажатии на кнопку отмена
+            && a !== '' && b !== ''
+            && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+// rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классный зритель');
+    } else if (personalMovieDB >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+// detectPersonalLevel();
+
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.private);
