34fdc9dfc70cb43efc5ae8a012a66d37fb6b0a76
Aleksandr Khozhainov <sea.man.aleksandr@gmail.com>
2020-08-26 00:15:18 +0300

    Finish lesson 25

--- Lesson__025/js/script.js
+++ Lesson__025/js/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const calc = new Calculator(10);
+
+calc.sum(5);
+calc.mult(10);
+calc.sub(40);
+calc.div(10);
+calc.set(100);
+calc.getResult();
+
+function Calculator(baseValue) {
+  this.expression = baseValue;
+
+  this.sum = (valueSum) => {
+    this.expression += ` + ${valueSum}`;
+    return `Sum = ${(baseValue += valueSum)}`;
+  };
+  this.sub = (valueSub) => {
+    this.expression += ` - ${valueSub}`;
+    return `Sub = ${(baseValue -= valueSub)}`;
+  };
+  this.div = (valueDiv) => {
+    this.expression += ` / ${valueDiv}`;
+    return `Div = ${(baseValue /= valueDiv)}`;
+  };
+  this.mult = (valueMult) => {
+    this.expression += ` * ${valueMult}`;
+    return `Mult = ${(baseValue *= valueMult)}`;
+  };
+  this.set = (valueSet) => {
+    this.expression = valueSet;
+    return `Set = ${(baseValue = valueSet)}`;
+  };
+  this.getResult = () => `Total: ${this.expression} = ${baseValue}`;
+}
