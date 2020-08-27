d1cf1810d1dbcfeaa3b9538462a6c84ac2b1c060
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2020-08-27 23:08:07 +0300

    finish lesson 30

--- Lesson__030/js/script.js
+++ Lesson__030/js/script.js
@@ -1,26 +1,40 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
+const promoAdv = document.querySelectorAll('.promo__adv > img');
+const promoGenre = document.querySelector('.promo__genre');
+const promoBg = document.querySelector('.promo__bg');
+const promoInteractiveList = document.querySelector('.promo__interactive-list');
+const promoInteractiveItem = document.querySelectorAll('.promo__interactive-item');
+
+// 1) Удалить все рекламные блоки.
+promoAdv.forEach((item) => item.remove());
+
+// 2) Изменить жанр фильма.
+promoGenre.textContent = 'драма';
+
+// 3) Изменить задний фон постера.
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+
+// 4) Список фильмов на странице.
+// 5) Добавить нумерацию
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    'Лоан',
+    'Лига справедливости',
+    'Ла-ла лэнд',
+    'Одержимость',
+    'Скотт Пилигрим против...',
+  ],
 };
 
+promoInteractiveList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((films, item) => {
+  promoInteractiveList.innerHTML += `
+  <li class="promo__interactive-item">
+    ${item + 1} ${films}
+    <div class="delete"></div>
+  </li>`;
+});
