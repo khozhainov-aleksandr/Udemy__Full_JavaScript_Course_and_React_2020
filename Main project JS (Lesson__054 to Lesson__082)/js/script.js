cb2cd0b6f7074b810caec72f556e7162ad5d1b89
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2021-03-06 13:04:06 +0200

    Small example 'Fetch API' delete this pice of code, after commit.

--- Main project JS (Lesson__054 to Lesson__082)/js/script.js
+++ Main project JS (Lesson__054 to Lesson__082)/js/script.js
@@ -334,3 +334,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	// TOPIC: Fetch API
 
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify({name: 'AleX'}),
+		headers: {
+			'Content-type': 'application/json'
+		}
+	})
+	.then(response => response.json())
+	.then(json => console.log(json));
+
+});
