48c751be4aa0ad7a2e27e841bc658b399de23dfd
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2021-03-07 23:26:27 +0200

    Lesson 57, temporary stop to reduce method

--- Lesson__057 (Array methods)/script.js
+++ Lesson__057 (Array methods)/script.js
@@ -1,2 +1,36 @@
 'use strict';
 
+// TOPIC: Filter
+
+// const names = ['Alex', 'Ivan', 'Dima', 'Tolic', 'Ayfnjliy'];
+
+// const filterName = names.filter((name) => {
+// NOTE: Возвращает имена только те которые короче 5 символов.
+// 	return name.length < 5;
+// });
+
+// console.log(filterName);
+
+// TOPIC: Map
+
+// const answers = ['AleX', 'IvAN', 'jOhN'];
+
+// const result = answers.map(item => {
+// 	// NOTE: Все имена написаны в нижнем регистре.
+// 	return item.toLowerCase();
+// });
+
+// console.log(result);
+
+// TOPIC: every/some
+
+// const some = [4, 'asdf', 'sdafsafadf'];
+
+// NOTE: some возвращает true всегда когда хоть один элемент соответствует условию.
+// console.log(some.some(item => typeof(item) === 'number'));
+
+// NOTE: every возвращает true только тогда, когда все элементы попадают под условие. 
+// console.log(some.every(item => typeof(item) === 'number'));
+
+// TOPIC: reduce
+
