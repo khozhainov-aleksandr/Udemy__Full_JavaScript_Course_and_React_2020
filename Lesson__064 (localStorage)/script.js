de9b400a4adc51d27813677d0499841260ba6a9f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2021-03-14 18:29:20 +0200

    Finish lesson 64 (localStorage)

--- Lesson__064 (localStorage)/script.js
+++ Lesson__064 (localStorage)/script.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const checkbox = document.querySelector('#checkbox');
+const form = document.querySelector('form');
+const change = document.querySelector('#color');
+
+checkedSetCheckbox();
+checkedColorForm();
+
+checkbox.addEventListener('change', setCheckbox);
+change.addEventListener('click', changeFormColor);
+
+// ---------- Function ---------- //
+
+function setCheckbox() {
+	localStorage.setItem('isChecked', true);
+}
+
+function checkedSetCheckbox() {
+	if (localStorage.getItem('isChecked')) {
+		checkbox.checked = true;
+	}
+}
+
+function changeFormColor() {
+	if (localStorage.getItem('bg') === 'changed') {
+		localStorage.removeItem('bg');
+		form.style.backgroundColor = 'width';
+	} else {
+		localStorage.setItem('bg', 'changed');
+		form.style.backgroundColor = 'red';
+	}
+}
+
+function checkedColorForm() {
+	if (localStorage.getItem('bg') === 'changed') {
+		form.style.backgroundColor = 'red';
+	}
+}
